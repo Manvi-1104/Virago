8e73b8d3a580dbea15673d0463349533eea47fa2
Manvi-1104 <147900350+Manvi-1104@users.noreply.github.com>
2024-03-15 22:37:23 +0530

    Front end commit

---
+++ src/Components/Navbar.js
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import '@fortawesome/fontawesome-free/css/all.css';
+import { Button } from './Button';
+import './Navbar.css';
+
+function Navbar() {
+  const [click, setClick] = useState(false);
+  const closeMobileMenu=()=> setClick(false);
+  const [button, setButton]=useState(true);
+  const showButton = () =>{
+    if(window.innerWidth<=960){
+        setButton(false)
+    }else{
+        setButton(true)
+    }
+  };
+
+  useEffect(()=>{
+    showButton()
+  },[]);
+  window.addEventListener('resize',showButton);
+  return (
+    <>
+      <nav className='navbar'>
+        <div className='navbar-container'>
+          <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
+            Virago
+          </Link>
+          <div className='menu-icon' onClick={() => setClick(!click)}>
+            <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+          </div>
+          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+            <li className='nav-item'>
+                <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                    Home
+                </Link>
+            </li>
+            <li className='nav-item'>
+              <Link
+                to='/services'
+                className='nav-links'
+                onClick={closeMobileMenu}
+              >
+                Financial Services
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link
+                to='/products'
+                className='nav-links'
+                onClick={closeMobileMenu}
+              >
+                Community
+              </Link>
+            </li>
+
+          </ul>
+          {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>} 
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
